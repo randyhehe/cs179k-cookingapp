4a2cbff4839af6d07f949e4a975a903b339bf7d1
mmore022 <mmore022@ucr.edu>
2018-01-22 14:12:04 -0800

    testing commit

--- app/src/main/java/com/example/randyhe/cookpad/MainActivity.java
+++ app/src/main/java/com/example/randyhe/cookpad/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //tests
-        // 1 2 3
+        //1 2 3
     }
 }
